72ba5e768028ad46f61c729003f6474410dd735c
Adam Gardner <adam@agardner.net>
2022-05-04 16:24:47 +1000

    Update index.v2.php

--- index.v2.php
+++ index.v2.php
@@ -1,9 +1,18 @@
 <?php
   $delay = 5;
   sleep($delay);
-  $environment = getenv("DT_RELEASE_STAGE");
-  $version = getenv("DT_RELEASE_VERSION");
-  echo "Version: $version<br />";
-  echo "Delay: $delay second(s)<br />";
-  echo "Environment: $environment";
+  //$environment = getenv("DT_RELEASE_STAGE");
+  //$version = getenv("DT_RELEASE_VERSION");
+  //echo "Version: $version<br />";
+  //echo "Delay: $delay second(s)<br />";
+  //echo "Environment: $environment";
 ?>
+<!doctype html>
+<head>
+  <title></title>
+</head>
+<body>
+  <img width="100%" src="https://github.com/agardnerIT/OddFiles/raw/images/optus_broken.png" />
+</body>
+</html>
+

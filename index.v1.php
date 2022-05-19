46cb63a3d57eabc0ecdce46b2c103873198d9378
Adam Gardner <adam@agardner.net>
2022-05-19 13:24:36 +1000

    Update index.v1.php

--- index.v1.php
+++ index.v1.php
@@ -1,9 +1,18 @@
 <?php
   $delay = 0;
   sleep($delay);
-  $environment = getenv("DT_RELEASE_STAGE");
-  $version = getenv("DT_RELEASE_VERSION");
-  echo "Version: $version<br />";
-  echo "Delay: $delay second(s)<br />";
-  echo "Environment: $environment";
+  //$environment = getenv("DT_RELEASE_STAGE");
+  //$version = getenv("DT_RELEASE_VERSION");
+  //echo "Version: $version<br />";
+  //echo "Delay: $delay second(s)<br />";
+  //echo "Environment: $environment";
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>v1</title>
+</head>
+<body>
+  <img src="working.png" width="100%"/>
+</body>
+</html>
